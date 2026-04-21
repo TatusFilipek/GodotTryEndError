7a347ec05d3eb64239348d50a54c5495854481b4
TatusFilipek <fifi.hyski@gmail.com>
2026-04-21 14:24:16 +0200

    Dodanie gracza i spowodowanie by mogl interaktowac ze swiatem

---
+++ scripts/player.gd
@@ -0,0 +1,34 @@
+extends CharacterBody2D
+
+const MovementSpeed = 150
+const gravityForce = 9.8
+const weight = 1
+
+const ALLMOVEMENTVARIABLE = 100
+
+var velocitySandBox = Vector2.ZERO
+
+#checking for input
+func _input(event) -> void:
+	if event.is_action_pressed("moveUp"):
+		Jump()
+
+# physics update
+func _physics_process(delta: float) -> void:
+	velocitySandBox.x = MovementDirection() * MovementSpeed
+	if not is_on_floor():
+		velocitySandBox += get_gravity();
+	
+	velocity = velocitySandBox * ALLMOVEMENTVARIABLE * delta
+	move_and_slide()
+
+func MovementDirection() -> float:
+	var movementDirection = Input.get_axis("moveLeft", "moveRight")
+	return movementDirection
+
+func Jump() -> void:
+	velocity.y = -500
+
+
+#TODO:
+	#Fix gravity
